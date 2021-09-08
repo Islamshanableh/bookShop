61dc5ca96f56c99360d482e3929689285e9f4f76
OdaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-09-08 20:33:40 +0300

    controller for auth inside it function of login

---
+++ backend/routers/controllers/authentication.js
@@ -0,0 +1,21 @@
+const userModel = require("../../db/models/user");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt")
+require("dotenv").config();
+
+const Login = (req, res) => {
+  const email = req.body.toLowerCase();
+  const password = req.password;
+  userModel.findOne({ email })
+  .then((result) => {
+    if (!result) {
+      res.json({
+        success: false,
+        message: "The email doesn't exist",
+      });
+    }
+   
+  });
+};
+
+module.exports = { Login };
