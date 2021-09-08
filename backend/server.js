21ae88a417343085163e6213c1760ee2a5e605fb
OdaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-09-08 23:10:42 +0300

    addRouterfavtoserver

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const db = require('./db/db');
 const userRouter = require("../backend/routers/routes/user")
 const loginRouter = require ("../backend/routers/routes/login")
+const favoriteRouter=require("../backend/routers/routes/favourite")
 const app = express();
 
 //routers
@@ -18,6 +19,7 @@ app.use(cors());
 app.use("/users",userRouter)
 app.use("/login",loginRouter)
 app.use("/books",booksRouter)
+app.use("/favorite",favoriteRouter)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server On ${PORT}`);
