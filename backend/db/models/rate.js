52c086b4233d6ca84fb38e44d6f5bdcebeaa7411
RashedMigdady <migdadyrashed@gmail.com>
2021-09-08 19:56:10 +0300

    create schema for rate file

--- backend/db/models/rate.js
+++ backend/db/models/rate.js
@@ -1,1 +1,7 @@
-const mongoose = require ("mongoose");
+const mongoose = require("mongoose");
+
+const rateSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.objectId, ref: "User" },
+  book: { type: mongoose.Schema.Types.objectId, ref: "Book" },
+  count: { type: Number }
+});
