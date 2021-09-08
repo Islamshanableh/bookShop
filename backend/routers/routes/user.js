388ffbb4ec215c95d1e00724a82f367fd8756f9d
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-08 20:52:15 +0300

    clean code

--- backend/routers/routes/user.js
+++ backend/routers/routes/user.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const { createNewUser } = require("../controllers/user");
 
-// define router
 const userRouter = express.Router();
 
 userRouter.post("/register", createNewUser);
 
 module.exports = userRouter;
-
