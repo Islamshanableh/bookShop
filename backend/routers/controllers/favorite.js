ff5100f2b860c5bab83e7970690e3cf1bec4e203
RashedMigdady <migdadyrashed@gmail.com>
2021-09-08 20:50:21 +0300

    create add to favorite function

--- backend/routers/controllers/favorite.js
+++ backend/routers/controllers/favorite.js
@@ -1,1 +1,29 @@
-const favoriteModel = require('../../db/models/favorite')
+const favoriteModel = require("../../db/models/favorite");
+
+const addToFavorite = (req, res) => {
+  const { userId, bookId } = req.body;
+
+  const favorite = new favoriteModel({
+    userId,
+    bookId,
+  });
+
+  favorite
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        success: true,
+        message: `Success book Added`,
+        book: result,
+      });
+    })
+    .catch((err) => {
+      res.status(409).json({
+        success: false,
+        message: `error! `,
+        err: err,
+      });
+    });
+};
+
+module.exports = { addToFavorite };
