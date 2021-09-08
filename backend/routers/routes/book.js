8ca1574c1ca1ec2a6eba46f1791282d62ed9d7f0
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-08 22:52:19 +0300

    creating getAllBooks  route  DONE

--- backend/routers/routes/book.js
+++ backend/routers/routes/book.js
@@ -8,6 +8,7 @@ getBookByName } = require("../controllers/book");
 const booksRouter = express.Router();
 
 booksRouter.post("/createbook", CreatNewBook);
+booksRouter.get("/allbooks", getAllBooks);
 
 
 module.exports = loginRouter;
