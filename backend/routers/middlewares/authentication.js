8d691f371fe9a3968e1f188c8cfea5e8eac89891
RashedMigdady <migdadyrashed@gmail.com>
2021-09-08 22:21:17 +0300

    create meddleware authentication

--- backend/routers/middlewares/authentication.js
+++ backend/routers/middlewares/authentication.js
@@ -1,2 +1,16 @@
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
+
+
+const authentication = (req , res , next)=> {
+
+    if (!req.headers.authorization)
+    {
+        return res.status(403).json({
+            success: false,
+            message: `Forbidden`
+        });
+
+    }
+
+}
