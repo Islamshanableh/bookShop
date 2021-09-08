d9825c44c4d1c8a80f2b8cb8f6887aa552eeef07
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-08 22:27:08 +0300

    import loginRouter in server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./db/db');
 const userRouter = require("../backend/routers/routes/user")
+const loginRouter = require ("../backend/routers/routes/login")
 const app = express();
 
 //routers
@@ -15,6 +16,7 @@ app.use(cors());
 
 //app routers
 app.use("/users",userRouter)
+app.use("/login",loginRouter)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log(`Server On ${PORT}`);
