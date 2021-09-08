0f877d1f3f0f5afd2ec651e2d2de00eb9e65985f
RashedMigdady <migdadyrashed@gmail.com>
2021-09-08 21:04:52 +0300

    create add to cart function

---
+++ backend/routers/controllers/cart.js
@@ -0,0 +1,29 @@
+const cartModel = require("../../db/models/cart");
+
+const addToCart = ()=>{
+    const {userId , bookId} = req.body;
+
+    const cart = new cartModel ({
+        userId,
+        bookId
+
+    });
+
+    cart.save().then((result)=>{
+        res.status(201).json({
+            success: true,
+            message: `Success book Added`,
+            book: result,
+          })
+    }).catch((err) => {
+        res.status(409).json({
+          success: false,
+          message: `error! `,
+          err: err,
+        });
+      });
+}
+
+module.exports = {addToCart}
+
+
