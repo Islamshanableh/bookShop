8f654f8bf2ca08fa9fde5559e8f8bd2018f010d2
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-09 21:58:52 +0300

    navigation componenet modify 2  done

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {userContext} from "../../App"
+// import {userContext} from "../../App"
+
 const Navigation = () => {
 	const token = useContext (userContext) 
 	return (<div className="App">
 {!token.token?(<div className="navigation">
-<Link to= "/signin" >login </Link>
-<Link to= "/signup" >signUp</Link>
+<Link to= "/login" >login </Link>
+<Link to= "/signUp" >Register</Link>
 <Link to= "/Home" >Home</Link>	
 <Link to= "/aboutUs" >aboutUs</Link></div>	)
 :(<div className="navigation">
