3ea09d150b0961060d05e48765c927590182e9e4
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-09 22:13:17 +0300

    create rate component

---
+++ frontend/src/components/rate/rate.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { Rating } from "react-simple-star-rating";
+
+import axios from "axios";
+
+export const Rate = ({ userId, book }) => {
+   
+  };
