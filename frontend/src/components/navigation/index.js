6e3f0059987dfb77468643034d88b3b0cdbf9ac4
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-09 21:53:51 +0300

    navigation componenet under processing

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -2,7 +2,21 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import {userContext} from "../../App"
 const Navigation = () => {
-	return <div className="App">Navigation component</div>;
+	return (<div className="App">
+{!token.token?(<div className="navigation">
+<Link to= "/signin" >login </Link>
+<Link to= "/signup" >signUp</Link>
+<Link to= "/Home" >Home</Link>	
+<Link to= "/aboutUs" >aboutUs</Link></div>	)
+:()
+
+
+
+
+
+
+
+	</div>)
 };
 
 export default Navigation;
