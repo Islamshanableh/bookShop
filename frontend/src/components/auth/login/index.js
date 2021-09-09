95b9ad49797139c798ea8f594f389f9978509387
OdaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-09-09 21:00:38 +0300

    creat input and button and take the value

--- frontend/src/components/auth/login/index.js
+++ frontend/src/components/auth/login/index.js
@@ -14,9 +14,23 @@ const Login=()=>{
   const tokenContext = useContext(userContext);
   
 
-return (<div>
-
-</div>)
-}
+  return (
+    <>
+  
+        <div >
+            <h2>Login:</h2>
+     <input type="email" placeholder="Email-here" onChange={(e) => {
+            setEmail(e.target.value);
+        }}/>
+      <br />
+      <input type="password" placeholder="Password Here" onChange={(e) => {
+            setPassword(e.target.value);
+        }}/>
+      <br />
+      <button onClick={checkValid}>Login</button>
+        </div>
+        {status}
+        </>
+    )
 
 export default Login
