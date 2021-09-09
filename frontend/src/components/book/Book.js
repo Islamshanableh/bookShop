7a9581a6b6bef4e0337c7ca7364e92791d1a61d1
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-09 22:07:33 +0300

    create useEffect to git AllBook

--- frontend/src/components/book/Book.js
+++ frontend/src/components/book/Book.js
@@ -4,5 +4,11 @@ import { Rate } from "../rating/rate";
 import axios from "axios";
 
 export const AllBook = () => {
-  
+    const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/books/allbooks").then((res) => {
+      setBooks([...res.data.books]);
+    });
+  }, []);
 };
