05134ef94c6a44af276e07018117126b77b26e51
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-09 21:52:18 +0300

    starting navigation componenet

--- frontend/src/components/navigation/index.js
+++ frontend/src/components/navigation/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import {userContext} from "../../App"
 const Navigation = () => {
 	return <div className="App">Navigation component</div>;
 };
