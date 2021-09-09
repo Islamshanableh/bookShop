2a29ff6dec2dc07de09a2734810f00156a07eab2
RashedMigdady <migdadyrashed@gmail.com>
2021-09-09 21:24:55 +0300

    create element and add onChange functions

--- frontend/src/components/auth/signUp/index.js
+++ frontend/src/components/auth/signUp/index.js
@@ -5,6 +5,56 @@ import axios from "axios";
 export const Register = () =>{
     return(
         <div>
+            <>
+        <form onSubmit={addNewUser}>
+          <br />
+          <input
+            type="text"
+            placeholder="First name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Last Name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Country"
+            onChange={(e) => setCountry(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            onChange={(e) => setPhoneNumber(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Birthday"
+            onChange={(e) => setBirthDate(e.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br />
+          <button>Register</button>
+          <br />
+        </form>
+        {message && <div>{message}</div>}
+      </>
 
         </div>
     )
