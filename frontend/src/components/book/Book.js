572bcfac6f41da420ae5a0ffaa0a2120a760f886
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-09 22:06:51 +0300

    create function AllBook

---
+++ frontend/src/components/book/Book.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import { Rate } from "../rating/rate";
+import axios from "axios";
+
+export const AllBook = () => {
+  
+};
