f7b8c8dd7a8e919709a32781831d3daa7842d301
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-11 21:48:09 +0300

    find favourite by user id function  modifiying the router

--- backend/routers/routes/favourite.js
+++ backend/routers/routes/favourite.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const {addToFavorite}=require("../controllers/favorite")
+const {addToFavorite,FindByUserId}=require("../controllers/favorite")
 const favoriteRouter=express.Router()
 const {authentication}=require("../middlewares/authentication")
 
