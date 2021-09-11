fb79ab6f1a431ab1993181737ce68a5ece1c9b96
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-11 20:14:51 +0300

    create function yo get value from droplist

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,21 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../App.css";
 
 export const Search = () => {
+  const [value, setValue] = useState("");
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
   return (
     <div className="App">
-      <select>
+      <select onChange={handleChange}>
         <option value="Select">Select</option>
         <option value="type">Type</option>
         <option value="name">Name</option>
         <option value="author">Author</option>
       </select>
-      <input
-        
-      ></input>
-      <button >
-        
-      </button>
+      <input></input>
+      <button></button>
     </div>
   );
 };
