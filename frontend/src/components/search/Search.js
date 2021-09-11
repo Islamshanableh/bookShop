f29671d1e912710b51d0b2dacc57109f89309c2b
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-11 20:19:22 +0300

    create function to get books on click search

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,11 +1,36 @@
 import React, { useState } from "react";
 import "../../App.css";
+import axios from "axios";
 
 export const Search = () => {
   const [value, setValue] = useState("");
   const [searchVal, setSearchVal] = useState("");
   const handleChange = (e) => {
     setValue(e.target.value);
+  };
+
+  const goSearch = () => {
+    if (value === "type") {
+      axios
+        .get(`http://localhost:5000/books/typeOfBook/${searchVal}`)
+        .then((res) => {
+          setBooks([...res.data.message]);
+        });
+    } else if (value === "name") {
+      axios
+        .get(`http://localhost:5000/books/nameOfBook/${searchVal}`)
+        .then((res) => {
+          setBooks([...res.data.message]);
+        });
+    } else if (value === "author") {
+      axios
+        .get(`http://localhost:5000/books/nameOfAuthor/${searchVal}`)
+        .then((res) => {
+          setBooks([...res.data.articles]);
+        });
+    } else {
+      setMessage("no books found");
+    }
   };
   return (
     <div className="App">
@@ -20,7 +45,7 @@ export const Search = () => {
           setSearchVal(e.target.value);
         }}
       ></input>
-      <button></button>
+      <button onClick={goSearch}></button>
     </div>
   );
 };
