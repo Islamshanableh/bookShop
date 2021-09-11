b1148609502cfa91da7e22eb7b590ea8c4f5fbf3
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-11 20:12:35 +0300

    create input and button for search

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -1,15 +1,21 @@
 import React from "react";
-
-
+import "../../App.css";
 
 export const Search = () => {
-  
-    
-  
-   
-     
-  
-    return (<div>
+  return (
+    <div className="App">
+      <select>
+        <option value="Select">Select</option>
+        <option value="type">Type</option>
+        <option value="name">Name</option>
+        <option value="author">Author</option>
+      </select>
+      <input
         
-    </div>)
-  };
+      ></input>
+      <button >
+        
+      </button>
+    </div>
+  );
+};
