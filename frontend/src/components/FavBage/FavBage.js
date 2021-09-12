527a9a584e8cf4f47d73651e3b046cbe265088b0
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-13 00:40:26 +0300

    starting  creating the favourite bage componeent

---
+++ frontend/src/components/FavBage/FavBage.js
@@ -0,0 +1,11 @@
+
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { userContext } from "../../App";
+
+export const  FavBage =()=> {
+
+
+
+    
+}
