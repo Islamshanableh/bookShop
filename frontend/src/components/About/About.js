90b853fa042c09f058d2e7736e4cfabdbf543f08
RashedMigdady <migdadyrashed@gmail.com>
2021-09-12 22:35:50 +0300

    ctreat all elements for about Modal

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,10 +1,8 @@
 import React from "react";
 import Modal from "react-modal";
 
-export const About = ()=> {
-
-
-    let subtitle;
+export const About = () => {
+  let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
 
   function openModal() {
@@ -12,16 +10,30 @@ export const About = ()=> {
   }
 
   function afterOpenModal() {
-    
-    subtitle.style.color = '#f00';
+    subtitle.style.color = "#f00";
   }
 
   function closeModal() {
     setIsOpen(false);
   }
-  return(
-      <div>
-          
-      </div>
-  )
-}
+  return (
+    <div>
+      <button onClick={openModal}>About Us</button>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+        ariaHideApp={false}
+      >
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>About Us</h2>
+
+        <div>
+          <p> we are students from meraki academy</p>
+          <button onClick={closeModal}>close</button>
+        </div>
+      </Modal>
+    </div>
+  );
+};
