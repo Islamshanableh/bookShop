bbbad3784823406a287b5849041ac9ca39f92250
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-13 00:20:51 +0300

    create delete from favourite route

--- backend/routers/routes/favourite.js
+++ backend/routers/routes/favourite.js
@@ -5,5 +5,5 @@ const {authentication}=require("../middlewares/authentication")
 
 favoriteRouter.post("/",authentication,addToFavorite)
 favoriteRouter.get("/getAllFav/",authentication,FindByUserId)
-
+favoriteRouter.delete("/:id",authentication,deleteBookById)
 module.exports = favoriteRouter;
