78ca9f66598de0b279136cf711f2a65c7ca91005
RashedMigdady <migdadyrashed@gmail.com>
2021-09-12 22:46:37 +0300

    craete all function to hadnle with contact Modal

--- frontend/src/components/Contact/Contact.js
+++ frontend/src/components/Contact/Contact.js
@@ -11,3 +11,29 @@ const customStyles = {
     transform: "translate(-50%, -50%)",
   },
 };
+
+export const Contact = () => {
+    let subtitle;
+    const [modalIsOpen, setIsOpen] = React.useState(false);
+  
+    function openModal() {
+      setIsOpen(true);
+    }
+  
+    function afterOpenModal() {
+      subtitle.style.color = "#f00";
+    }
+  
+    function closeModal() {
+      setIsOpen(false);
+    }
+
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+
+  
