54561bc126549ebdd37151a339621318f1214a66
odaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-09-12 08:28:08 +0300

    git userID by token and populat the bookID

--- backend/routers/controllers/cart.js
+++ backend/routers/controllers/cart.js
@@ -25,8 +25,23 @@ const addToCart = (req,res)=>{
       });
 }
 
+//.populate("author", "firstName lastName -_id")
+
 const FindByUserId = (req,res)=>{
-  
+  const userId = req.token.userId
+  cartModel.find({userId})
+  .populate("bookId")
+  .exec()
+  .then((result)=>{
+    if(!result.length){
+      return res.status(409).json({
+        success: false,
+        message: `Shopping cart is Empty`,
+        err: err,
+      })
+    }
+    
+  })
 }
 
 module.exports = {addToCart,FindByUserId}
