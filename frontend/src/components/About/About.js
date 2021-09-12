d3159cbd6b37f8961d7847d801e1e80a46622ad6
RashedMigdady <migdadyrashed@gmail.com>
2021-09-12 22:41:28 +0300

    add some style for about modal

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Modal from "react-modal";
 
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
 export const About = () => {
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
