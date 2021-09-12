84a2ecb4a1b6e74d4bbfdb3491624891e054ad3f
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-13 00:41:31 +0300

    creating the favourite bage componeent  under processing

--- frontend/src/components/FavBage/FavBage.js
+++ frontend/src/components/FavBage/FavBage.js
@@ -4,8 +4,12 @@ import axios from "axios";
 import { userContext } from "../../App";
 
 export const  FavBage =()=> {
+    const [book, setBook] = useState();
+    const [status, setStatus] = useState();
+
+    const state = useContext(userContext);
+    const token = state.token;
 
 
 
-    
 }
