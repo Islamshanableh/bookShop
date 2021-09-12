bc922f8cf4d256bf90711dd634f9c0c41540aa99
odaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-09-12 11:39:41 +0300

    creat button on click creat function

--- frontend/src/components/ShoppingCart/ShoppingCart.js
+++ frontend/src/components/ShoppingCart/ShoppingCart.js
@@ -4,6 +4,7 @@ import { userContext } from "../../App";
 
 export const ShoppingCart = () => {
   const [book, setBook] = useState();
+  const [price, setPrice] = useState([]);
   const state = useContext(userContext);
   const token = state.token;
 
@@ -19,21 +20,36 @@ export const ShoppingCart = () => {
       });
   }, [book]);
 
-  return <div>{!book ? <div>Shopping cart is Empty</div> : (<div>
-
-   {book.map((element,i)=>{
-       
-       return  <div key={element._id}>
-            <br></br> {element.bookId.image},<br></br> {element.bookId.name}
-              <br></br>
-              {element.bookId.type},<br></br>
-              {element.bookId.author},<br></br>
-              {element.bookId.description},<br></br>
-              {element.bookId.language},<br></br>
-              {element.bookId.price},<br></br>
-       </div>
-   })}
-             
-  </div>)}
-  </div>;
+  return (
+    <div>
+      {!book ? (
+        <div>Shopping cart is Empty</div>
+      ) : (
+        <div>
+          {book.map((element, i) => {
+              
+            return (
+              <div key={element._id}>
+                {element.bookId.image}
+                <br></br> 
+                {element.bookId.name}
+                <br></br>
+                {element.bookId.type}
+                <br></br>
+                {element.bookId.author}
+                <br></br>
+                {element.bookId.description}
+                <br></br>
+                {element.bookId.language}
+                <br></br>
+                {element.bookId.price}
+                <br></br>
+                <button onClick={()=>{}}>X</button>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
 };
