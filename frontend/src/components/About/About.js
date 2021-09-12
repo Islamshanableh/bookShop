d082472657c61563e431eeb16aa0ddb2c658ecf1
RashedMigdady <migdadyrashed@gmail.com>
2021-09-12 22:34:32 +0300

    create functions for modal

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -1,3 +1,27 @@
 import React from "react";
 import Modal from "react-modal";
 
+export const About = ()=> {
+
+
+    let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    
+    subtitle.style.color = '#f00';
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+  return(
+      <div>
+          
+      </div>
+  )
+}
