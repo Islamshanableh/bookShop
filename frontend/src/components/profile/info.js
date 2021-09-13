0b6119ff842fc3fae41ec330ed51ede1e850abe6
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-13 20:25:18 +0300

    define state for update

--- frontend/src/components/profile/info.js
+++ frontend/src/components/profile/info.js
@@ -3,46 +3,52 @@ import { userContext } from "../../App";
 import axios from "axios";
 
 export const Info = () => {
-    const [info, setInfo] = useState([]);
-    useEffect(() => {
-        axios
-          .get(
-            "http://localhost:5000/users",
-    
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          )
-          .then((res) => {
-            setInfo([...res.data.userInfo]);
-            
-          });
-      }, []);
-      const updateInfo = () => {
-        axios
-          .post(
-            "http://localhost:5000/users/update",
-            {
-              firstName: firstName,
-              lastName: lastName,
-              phoneNumber: phone,
-              BirthDate: date,
-              country: country,
-            },
-    
-            {
-              headers: {
-                Authorization: `Bearer ${token}`,
-              },
-            }
-          )
-          .then((res) => {
-            setInfo([...res.data.userInfo]);
-            setMessage(res.data.message)
-          });
-      };
-  return(
-      <div></div>
-  )}
+  const [info, setInfo] = useState([]);
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [country, setCountry] = useState();
+  const [phone, setPhone] = useState();
+  const [date, setDate] = useState();
+  const [message, setMessage] = useState("");
+  const state = useContext(userContext);
+  const token = state.token;
+  useEffect(() => {
+    axios
+      .get(
+        "http://localhost:5000/users",
+
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        setInfo([...res.data.userInfo]);
+      });
+  }, []);
+  const updateInfo = () => {
+    axios
+      .post(
+        "http://localhost:5000/users/update",
+        {
+          firstName: firstName,
+          lastName: lastName,
+          phoneNumber: phone,
+          BirthDate: date,
+          country: country,
+        },
+
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      )
+      .then((res) => {
+        setInfo([...res.data.userInfo]);
+        setMessage(res.data.message);
+      });
+  };
+  return <div></div>;
+};
