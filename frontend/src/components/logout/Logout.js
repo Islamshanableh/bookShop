8c0a08d9c5edc368bf5848b12d067f893e066b2d
OdaiJawabreh <odai.jawabreh.oj@gmail.com>
2021-09-13 13:08:17 +0300

    log out done

--- frontend/src/components/logout/Logout.js
+++ frontend/src/components/logout/Logout.js
@@ -1,8 +1,12 @@
-
+import React from "react";
 import { useHistory } from "react-router-dom";
 
 export const Logout=()=>{
     const history = useHistory()
     localStorage.clear()
     history.push("/login")
+    return (<div>
+        
+    </div>)
+
 }
