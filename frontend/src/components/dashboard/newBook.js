c5e1a2b3f0bb76036f05813b621a0f3e8a79ab30
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-13 20:40:01 +0300

    create newBook component

---
+++ frontend/src/components/dashboard/newBook.js
@@ -0,0 +1,7 @@
+import React, { useState, useContext } from "react";
+import { userContext } from "../../App";
+import axios from "axios";
+export const NewBook = () => {
+ return(
+     <div></div>
+ )}
