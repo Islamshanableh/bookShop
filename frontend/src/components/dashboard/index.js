b37b2c7fa09dd94b26de4b22df282b724de744b0
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-13 20:31:59 +0300

    create git all books that user created

--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useContext, useEffect } from "react";
+import { userContext } from "../../App";
+import axios from "axios";
 
 const Dashboard = () => {
+	const getAll = ()=>{
+		axios
+		.get(
+		  "http://localhost:5000/books/myBook",
+	
+		  {
+			headers: {
+			  Authorization: `Bearer ${token}`,
+			},
+		  }
+		)
+		.then((res) => {
+		   setBook([...res.data.book]);
+		  
+		});
+	  }
 	return <div className="App">Dashboard component</div>;
 };
 
