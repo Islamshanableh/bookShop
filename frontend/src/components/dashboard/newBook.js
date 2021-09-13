c9913dd3c4a038bab597315343cce1c34605a418
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-13 20:40:43 +0300

    create function to add new book

--- frontend/src/components/dashboard/newBook.js
+++ frontend/src/components/dashboard/newBook.js
@@ -2,6 +2,26 @@ import React, { useState, useContext } from "react";
 import { userContext } from "../../App";
 import axios from "axios";
 export const NewBook = () => {
+    const addNewBook = () => {
+        axios
+          .post(
+            "http://localhost:5000/books/createbook",
+            { image, name, type, author, language, description,price },
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          )
+          .then((res) => {
+            setSuccsess("The book has been created successfully");
+          })
+          .catch((err) => {
+            setSuccsess(
+              "Error happened while creating a new book, please try again"
+            );
+          });
+      };
  return(
      <div></div>
  )}
