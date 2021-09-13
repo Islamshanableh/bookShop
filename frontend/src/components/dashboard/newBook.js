2135845f45285f6deed0be395af8e45155a6a630
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-13 20:44:16 +0300

    newBook done

--- frontend/src/components/dashboard/newBook.js
+++ frontend/src/components/dashboard/newBook.js
@@ -4,14 +4,15 @@ import axios from "axios";
 export const NewBook = () => {
   const state = useContext(userContext);
   const token = state.token;
-  const [image, setImage] = useState();
-  const [name, setName] = useState();
-  const [type, setType] = useState();
-  const [author, setAuthor] = useState();
-  const [language, setLanguage] = useState();
-  const [price, setPrice] = useState();
-  const [description, setDescription] = useState();
+  const [image, setImage] = useState(0);
+  const [name, setName] = useState(0);
+  const [type, setType] = useState(0);
+  const [author, setAuthor] = useState(0);
+  const [language, setLanguage] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState(0);
   const [success, setSuccsess] = useState(null);
+
   const addNewBook = () => {
     axios
       .post(
