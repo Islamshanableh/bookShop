1059e42a7b59c860e3c5a908bfba577c6f89bb6c
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-13 23:43:47 +0300

    creating the footer

---
+++ frontend/src/components/footer/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function footer() {
+    return (
+        <div>
+        <div className="folllowUs">
+            follow Us on 
+            </div>    
+        </div>
+    )
+}
+
+export default footer
