679f7600e44ebdf73f26f8233a084c56aeff32e3
G11_Kulthoom <kulthumradi94@gmail.com>
2021-09-13 18:05:09 +0300

    starting favourite component

---
+++ frontend/src/components/favourite/favourite.js
@@ -0,0 +1,11 @@
+import React, { useState,useContext } from "react";
+import { Route } from "react-router-dom";
+import { userContext } from "../../App";
+import axios from "axios";
+
+
+function Favourite({bookId}) {}
+
+
+
+export default Favourite
