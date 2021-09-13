ad29f6b000b97e682b96ce367a4a9d4e8786cedc
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-13 20:33:42 +0300

    create states to book information

--- frontend/src/components/dashboard/index.js
+++ frontend/src/components/dashboard/index.js
@@ -3,6 +3,17 @@ import { userContext } from "../../App";
 import axios from "axios";
 
 const Dashboard = () => {
+	const state = useContext(userContext);
+	const token = state.token;
+	const [book, setBook] = useState([]);
+	const [image, setImage] = useState();
+	const [name, setName] = useState();
+	const [type, setType] = useState();
+	const [author, setAuthor] = useState();
+	const [language, setLanguage] = useState();
+	const [price, setPrice] = useState();
+	const [description, setDescription] = useState();
+	const [success, setSuccsess] = useState(null);
 	const getAll = ()=>{
 		axios
 		.get(
