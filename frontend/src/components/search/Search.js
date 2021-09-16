4b0e675a33aba0918bf35936db9173abdc7f09ab
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-17 00:03:14 +0300

    edit on search input

--- frontend/src/components/search/Search.js
+++ frontend/src/components/search/Search.js
@@ -41,19 +41,17 @@ export const Search = () => {
 
   return (
     <div className="App">
-      <select onChange={handleChange}>
+       <div className="searchBox">
+        <select onChange={handleChange}>
         <option value="Select">Select</option>
         <option value="type">Type</option>
         <option value="name">Name</option>
         <option value="author">Author</option>
       </select>
-      <input
-        onChange={(e) => {
-          setSearchVal(e.target.value);
-        }}
-      ></input>
-      <button onClick={goSearch}>
-        <svg
+
+<input className="searchInput"type="text" name="" placeholder="Search"/>
+<button className="searchButton" href="#">
+<svg
           xmlns="http://www.w3.org/2000/svg"
           width="16"
           height="16"
@@ -63,7 +61,8 @@ export const Search = () => {
         >
           <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
         </svg>
-      </button>
+</button>
+</div>
       <div>
         <p>{message}</p>
       </div>
