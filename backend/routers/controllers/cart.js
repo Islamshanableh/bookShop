a7ea57994bcb50ac6d2ac750b6ab0f91ee82f72d
Islamshanableh <islam.alshanableh@gmail.com>
2021-09-18 18:17:32 +0300

    clear log for backend

--- backend/routers/controllers/cart.js
+++ backend/routers/controllers/cart.js
@@ -1,71 +1,70 @@
 const cartModel = require("../../db/models/cart");
-const userModel = require("../../db/models/user")
-const x = 1
-const addToCart = (req,res)=>{
- 
-    const {bookId} = req.body;
-    const userId = req.token.userId
-    
-    const cart = new cartModel ({
-        userId,
-        bookId
+const userModel = require("../../db/models/user");
+const x = 1;
+const addToCart = (req, res) => {
+  const { bookId } = req.body;
+  const userId = req.token.userId;
 
-    });
+  const cart = new cartModel({
+    userId,
+    bookId,
+  });
 
-    cart.save().then(async(result)=>{
-     await userModel.findByIdAndUpdate(userId,{$push:{"cart" : bookId}})
-        res.status(201).json({
-            success: true,
-            message: `Success book Added`,
-            book: result,
-          })
-    }).catch((err) => {
-        res.status(409).json({
-          success: false,
-          message: `error! `,
-          err: err,
-        });
+  cart
+    .save()
+    .then(async (result) => {
+      await userModel.findByIdAndUpdate(userId, { $push: { cart: bookId } });
+      res.status(201).json({
+        success: true,
+        message: `Success book Added`,
+        book: result,
       });
-}
-
-const FindByUserId = (req,res)=>{
-  const userId = req.token.userId
-  cartModel.find({userId})
-  .populate("bookId","image name type author description language price _id")
-  .exec()
-  .then((result)=>{
-    console.log(result);
-    if(!result.length){
-      console.log("fffffffffffffffffffffffffffffffff");
-      return res.status(409).json({
-        success: false,
-        message: result,
-      })
-    }
-    res.json({
-      succes: true,
-      message: result
-    });
-  }).catch((err)=>{
-    res.json({
-      success:false,
-      message: "server error"
     })
-  
-  })
-}
+    .catch((err) => {
+      res.status(409).json({
+        success: false,
+        message: `error! `,
+        err: err,
+      });
+    });
+};
+
+const FindByUserId = (req, res) => {
+  const userId = req.token.userId;
+  cartModel
+    .find({ userId })
+    .populate("bookId", "image name type author description language price _id")
+    .exec()
+    .then((result) => {
+      if (!result.length) {
+        return res.status(409).json({
+          success: false,
+          message: result,
+        });
+      }
+      res.json({
+        succes: true,
+        message: result,
+      });
+    })
+    .catch((err) => {
+      res.json({
+        success: false,
+        message: "server error",
+      });
+    });
+};
 
 const deleteBookById = (req, res) => {
-  
-const id = req.params.id; 
-const {bookId }= req.body
- const userId = req.token.userId
+  const id = req.params.id;
+  const { bookId } = req.body;
+  const userId = req.token.userId;
   cartModel
     .findByIdAndDelete(id)
-    .then(async(result) => {
-      console.log(`1=>${result}`);
-   await userModel.findByIdAndUpdate(userId,{$pull:{"cart" : bookId}})
-   .then((result)=>{console.log(`2=>${result}`);})
+    .then(async (result) => {
+      await userModel
+        .findByIdAndUpdate(userId, { $pull: { cart: bookId } })
+        .then((result) => {});
       if (!result) {
         return res.status(404).json({
           success: false,
@@ -86,6 +85,4 @@ const {bookId }= req.body
     });
 };
 
-module.exports = {addToCart,FindByUserId,deleteBookById}
-
-
+module.exports = { addToCart, FindByUserId, deleteBookById };
