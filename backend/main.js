c329fcbb3758d07938dfe5965d1f0758b4a573e3
Islamshanableh <islam.alshanableh@gmail.com>
2021-11-03 22:09:01 +0200

    divide server for testing

---
+++ backend/main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+
+
+const userRouter = require("../backend/routers/routes/user")
+const loginRouter = require ("../backend/routers/routes/login")
+const favoriteRouter=require("../backend/routers/routes/favourite")
+const booksRouter = require("../backend/routers/routes/book")
+const ratingRouter = require("../backend/routers/routes/rate")
+const cartRouter = require("../backend/routers/routes/cart")
+const app = express();
+
+//routers
+
+//built-in middleware
+app.use(express.json());
+
+//third-party middleware
+app.use(cors());
+
+
+//app routers
+app.use("/users",userRouter)
+app.use("/login",loginRouter)
+app.use("/books",booksRouter)
+app.use("/favorite",favoriteRouter)
+app.use("/rate",ratingRouter)
+app.use("/cart",cartRouter)
+
+
+module.exports = app;
